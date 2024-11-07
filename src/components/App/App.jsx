6328e5dd90bcd14460da0597cbfea693140c9ac9
jakanoh <jakanoh17@gmail.com>
2024-11-07 15:13:01 -0500

    add base page components

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,8 +2,12 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import HeroSection from "../HeroSection/HeroSection";
-import ContentSections from "../ContentSections/ContentSections";
 import Navbar from "../Navbar/Navbar";
+import EventTypes from "../EventTypes/EventTypes";
+import EventCal from "../EventCal/EventCal";
+import AboutUs from "../AboutUs/AboutUs";
+import Blog from "../Blog/Blog";
+import Volunteer from "../Volunteer/Volunteer";
 
 function App() {
   return (
@@ -11,7 +15,11 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<HeroSection />} />
-        <Route path="/contents" element={<ContentSections />} />
+        <Route path="/event-types" element={<EventTypes />} />
+        <Route path="/event-calendar" element={<EventCal />} />
+        <Route path="/about-us" element={<AboutUs />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/volunteer" element={<Volunteer />} />
       </Routes>
     </Router>
   );
