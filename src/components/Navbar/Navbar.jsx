b7482d23a1417d7107791b1fa9a583461940397d
jakanoh <jakanoh17@gmail.com>
2024-11-06 20:14:22 -0500

    add link functionality to navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -11,12 +11,28 @@ import { navbar, navbar__menu, navbar__icon } from "./Navbar.module.css";
 =======
 import logo from "../../assets/rough_logo.png";
 import "./Navbar.css";
+<<<<<<< HEAD
 >>>>>>> 146e048 (add logo and final fixes for nav menu)
+=======
+import { Link, useNavigate } from "react-router-dom";
+>>>>>>> 5f7e0dd (add link functionality to navbar)
 
 function Navbar() {
+  const navigate = useNavigate();
+
+  function navigateToEventTypesPage() {
+    navigate("/event-types");
+  }
+
+  function navigateToEventCalPage() {
+    navigate("/event-calendar");
+  }
+
   return (
     <nav className="navbar">
-      <img src={logo} alt="BCC logo" className="navbar__icon" />
+      <Link to="/">
+        <img src={logo} alt="BCC logo" className="navbar__icon" />
+      </Link>
       <ul className="navbar__menu">
         <li className="navbar__menu-item">
           <BasicMenu
@@ -24,15 +40,39 @@ function Navbar() {
             menuId="navEventsMenu"
             buttonTitle="Event"
             menuItems={[
-              { name: "Type of Events", id: "type" },
-              { name: "Event Calendar", id: "calendar" },
+              {
+                name: "Type of Events",
+                id: "type",
+                handleClick: navigateToEventTypesPage,
+              },
+              {
+                name: "Event Calendar",
+                id: "calendar",
+                handleClick: navigateToEventCalPage,
+              },
             ]}
           />
         </li>
-        <li className="navbar__menu-item">About Us</li>
-        <li className="navbar__menu-item">Blog</li>
-        <li className="navbar__menu-item">Sponsor</li>
-        <li className="navbar__menu-item">Volunteer</li>
+        <li className="navbar__menu-item">
+          <Link className="navbar__link" to="/about-us">
+            About Us
+          </Link>
+        </li>
+        <li className="navbar__menu-item">
+          <Link className="navbar__link" to="/blog">
+            Blog
+          </Link>
+        </li>
+        <li className="navbar__menu-item">
+          <Link className="navbar__link" to="/sponsors">
+            Sponsor
+          </Link>
+        </li>
+        <li className="navbar__menu-item">
+          <Link className="navbar__link" to="/volunteer">
+            Volunteer
+          </Link>
+        </li>
       </ul>
     </nav>
   );
