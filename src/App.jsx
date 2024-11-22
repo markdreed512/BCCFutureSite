e7acc20f1b74f072ddef057e41eab3c7ed1e959e
jakanoh <jakanoh17@gmail.com>
2024-11-21 20:22:24 -0500

    fix app files

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,20 @@
-import React from "react"
-import {BrowserRouter as Router, Route, Routes} from "react-router-dom"
-import Navbar from "./components/Navbar"
-import HomeView from './components/HomeView/HomeView'
-import Footer from './components/Footer/Footer'
-import './App.css'
-
+// src/App.jsx
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
+import HeroSection from "../HeroSection/HeroSection";
+import ContentSections from "../ContentSections/ContentSections";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
-	return (
-		<Router>
-			<Navbar />
-			<Routes>
-				<Route path="/" element={<HomeView />} />
-			</Routes>
-			<Footer />
-		</Router>
-	)
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HeroSection />} />
+        <Route path="/contents" element={<ContentSections />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
