7f83ff6f0b7acc89196fd3eff5166c5630c26405
valentineokundaye <valentineokundaye21@gmail.com>
2024-12-04 19:53:28 -0500

    slide movement

--- src/components/Carousel/Slides.js
+++ src/components/Carousel/Slides.js
@@ -1,12 +1,16 @@
-import regular_meeting from '../../assets/carouselImages/regular_meeting.png'
+import regular_meeting from '../../assets/carouselImages/regular_meeting.jpg'
 import workshop from '../../assets/carouselImages/workshop.png'
-import lightning_talk from '../../assets/carouselImages/lightning_talk.png'
-
+import lightning_talk from '../../assets/carouselImages/lightning_talk.jpg'
+import demo_day from '../../assets/carouselImages/demo_day.jpg'
+import symposium from '../../assets/carouselImages/symposium.jpg'
 
 const Slides = [
-    {"image": regular_meeting, "title": "Regular Meeting", "route": '#'}, 
-    {"image": workshop, "title": "Workshop", "route": '#'},
-    {"image": lightning_talk, "title": "Lightning Talk", "route": '#'}
+    {"image": regular_meeting, "title": "Regular Meetings", "route": '#'}, 
+    {"image": demo_day, "title": "Demo Days", "route": '#'},
+    {"image": workshop, "title": "Workshops", "route": '#'},
+    {"image": lightning_talk, "title": "Lightning Talks", "route": '#'},
+    {"image": symposium, "title": "Symposiums", "route": '#'}
+    
 ]
 
 export default Slides
