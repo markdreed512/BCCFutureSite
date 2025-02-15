005ba113f8b9800acbedaf60e9725a9d4f0f92c4
Brent Gauthier <cajundrummer@gmail.com>
2025-02-15 12:42:34 -0500

    Add effect to scroll users to specific event type when selected from carousel

--- src/components/EventTypes/EventTypes.jsx
+++ src/components/EventTypes/EventTypes.jsx
@@ -1,8 +1,22 @@
+import { useLocation } from 'react-router-dom';
 import EventTypeCard from '../EventTypeCard/EventTypeCard';
 import EventTypeData from './EventTypeData'
 import "./EventTypes.css";
+import { useEffect } from 'react';
 
 export default function EventTypes() {
+  const location = useLocation(); // Get the current URL
+
+  useEffect(() => {
+    if (location.pathname.startsWith('/event-types/')) {
+      const tag = location.pathname.split('/').pop(); // Extract tag from URL
+      const element = document.getElementById(tag);
+      if (element) {
+        element.scrollIntoView({behavior: 'smooth'});
+      }
+    }
+  }, [location]);
+
   return(
     <div className="event-types__container">
       <div className="event-types__header">
