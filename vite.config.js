772fdf04747b9bfd23dd45bd4a552392511ab243
Valentine Okundaye <107582338+Valtaire-O@users.noreply.github.com>
2025-03-05 19:14:09 -0500

    changed base and replaced build with dist

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
-    base: './',
+    base: '/BaltimoreCode-Coffee.github.io/',
     // Other build configuration options...
   },
   
